d3d49d2f8b66736478ab43b1c7e7ab790c8eb878
David Leon <ndleon61@gmail.com>
2025-05-03 01:03:31 +0000

    Finish Project

--- src/app.js
+++ src/app.js
@@ -4,8 +4,29 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+  let cardSymbol = [
+    { name: "Hearts", icon: "fa-heart" },
+    { name: "Diamonds", icon: "fa-gem" },
+    { name: "Clubs", icon: "fa-leaf" },
+    { name: "Spades", icon: "fa-shield-alt" }
+  ];
+let number = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, "Queen", "King", "Knight"];
+
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Randomize number and symbols
+
+  let randomSymbol = cardSymbol[Math.floor(Math.random() * cardSymbol.length)];
+  let randomNumber = number[Math.floor(Math.random() * number.length)];
+
+  //Display the random number
+  document.getElementById('cardNumber').innerText = `${randomNumber}`;
+
+  // Display the random symbols
+  let topIcon = document.getElementById('topIcon');
+  let btnIcon = document.getElementById('bottomIcon');
+
+  topIcon.className = `fas ${randomSymbol.icon}`;
+  bottomIcon.className = `fas ${randomSymbol.icon}`;
+
 };
