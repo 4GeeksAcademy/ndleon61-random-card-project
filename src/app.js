e13c28a48b3f1199426c02b5c5f49e39bcd5a1ee
David Leon <ndleon61@gmail.com>
2025-05-06 00:46:39 +0000

    Added a button to generate a random card and more CSS styling

--- src/app.js
+++ src/app.js
@@ -4,29 +4,37 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-  let cardSymbol = [
-    { name: "Hearts", icon: "fa-heart" },
-    { name: "Diamonds", icon: "fa-gem" },
-    { name: "Clubs", icon: "fa-leaf" },
-    { name: "Spades", icon: "fa-shield-alt" }
-  ];
+
+let cardSymbol = [
+  { name: "Hearts", icon: "fa-heart", class: "symbol-red"},
+  { name: "Diamonds", icon: "fa-gem", class: "symbol-pink" },
+  { name: "Clubs", icon: "fa-leaf", class: "symbol-green" },
+  { name: "Spades", icon: "fa-shield-alt", class: "symbol-yellow" }
+];
 let number = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, "Queen", "King", "Knight"];
 
+const card = document.getElementById('card');
 
-window.onload = function() {
-  //Randomize number and symbols
-
+function randomCard() {
   let randomSymbol = cardSymbol[Math.floor(Math.random() * cardSymbol.length)];
   let randomNumber = number[Math.floor(Math.random() * number.length)];
 
-  //Display the random number
   document.getElementById('cardNumber').innerText = `${randomNumber}`;
 
-  // Display the random symbols
   let topIcon = document.getElementById('topIcon');
-  let btnIcon = document.getElementById('bottomIcon');
+  let bottomIcon = document.getElementById('bottomIcon');
 
-  topIcon.className = `fas ${randomSymbol.icon}`;
-  bottomIcon.className = `fas ${randomSymbol.icon}`;
+  topIcon.className = `fas ${randomSymbol.icon} ${randomSymbol.class}`;
+  bottomIcon.className = `fas ${randomSymbol.icon} ${randomSymbol.class}`;
+}
 
+window.onload = function () {
+  randomCard(); // show a card when the page loads
 };
+
+document.getElementById('button').addEventListener('click', function () {
+  card.classList.remove('animate-card');
+  void card.offsetWidth;
+  card.classList.add('animate-card');
+  randomCard();
+});
